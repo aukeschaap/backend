24356d5769343b67a856c1cb446df6968b015cfb
Auke Schaap <auke.schaap@gmail.com>
2025-06-11 21:41:37 +0200

    Enable CORS

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use axum::{Extension, Router};
 use std::sync::Arc;
 use sysinfo::System;
 use tokio::sync::Mutex;
+use tower_http::cors::{Any, CorsLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod server;
@@ -13,6 +14,12 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer()) // print logs to stdout
         .init();
 
+    // CORS middleware
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods([axum::http::Method::GET])
+        .allow_headers([axum::http::header::CONTENT_TYPE]);
+
     // Initialize system information
     let sys = Arc::new(Mutex::new(System::new_all()));
     let disks = Arc::new(Mutex::new(sysinfo::Disks::new_with_refreshed_list()));
@@ -20,7 +27,8 @@ async fn main() {
 
     let app = Router::new()
         .nest("/server", server::routes::routes())
-        .layer(Extension(state));
+        .layer(Extension(state))
+        .layer(cors);
 
     // run app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
